61d1df8a3790746e410995ed38d49a4b6569472a
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-12-07 13:20:02 +0000

    Updated register to be post instead of get request

--- API/Router/API.js
+++ API/Router/API.js
@@ -6,6 +6,6 @@ var apiController = require("../Controllers/API");
 
 router.get("/login", (req,res) => {apiController.login(req,res)});
 router.get("/logout", (req,res) => {apiController.logout(req,res)});
-router.get("/register", (req,res) => {apiController.register(req,res)});
+router.post("/register", (req,res) => {apiController.register(req,res)});
 
 module.exports = router;
