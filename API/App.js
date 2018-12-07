733e7fe3347002710f1d04d1b3a4f3f3c1817269
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-12-07 13:07:21 +0000

    Added register.html

--- API/App.js
+++ API/App.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8081;
 
@@ -7,6 +8,9 @@ app.use(function(req,res,next){
   next();
 });
 
+app.use("/register", function(req,res){
+  res.sendFile(path.join(__dirname+'/Views/register.html'));
+});
 app.use("/api", apiRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
